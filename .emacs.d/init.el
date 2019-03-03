3be6e6803185c334bbcaf851bc198efaa2d84982
juacq97 <juacq97@gmail.com>
2019-03-02 23:58:44 -0700

    Org-capture en una pequeña ventana con "super + c;c"

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -392,6 +392,31 @@ telephone-line-secondary-right-separator 'telephone-line-tan-hollow-right)
 (use-package evil-magit
   :ensure t)
 
+;;=========================================================
+(defadvice org-capture-finalize
+(after delete-capture-frame activate)
+"Advise capture-finalize to close the frame"
+(if (equal "capture" (frame-parameter nil 'name))
+(delete-frame)))
+
+(defadvice org-capture-destroy
+(after delete-capture-frame activate)
+"Advise capture-destroy to close the frame"
+(if (equal "capture" (frame-parameter nil 'name))
+(delete-frame)))
+
+(use-package noflet
+:ensure t )
+(defun make-capture-frame ()
+"Create a new frame and run org-capture."
+(interactive)
+;(make-frame '((name . "capture")))
+(select-frame-by-name "capture")
+(delete-other-windows)
+(noflet ((switch-to-buffer-other-window (buf) (switch-to-buffer buf)))
+(org-capture)))
+
+
 ;; ==== yequake ====
 (use-package yequake
   :ensure t
@@ -404,6 +429,7 @@ telephone-line-secondary-right-separator 'telephone-line-tan-hollow-right)
       (alpha . 0.95)
       (frame-parameters . ((undecorated . t)
                            (skip-taskbar . t)
+			   (quote (name . "capture"))
                            (sticky . t)))))))
 
 (use-package markdown-mode
